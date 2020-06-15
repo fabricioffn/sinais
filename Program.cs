abef85db83dafc19df88ee5daaef502ac088272f
fabricio <fnmamao@gmail.com>
2020-06-15 11:17:45 -0300

    exercicio

--- Program.cs
+++ Program.cs
@@ -6,7 +6,23 @@ namespace sinais
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+              int numero;
+          Console.WriteLine("digite um numero:");
+          numero = Convert.ToInt16(Console.ReadLine());
+
+            if(numero > 0)
+            {
+                Console.WriteLine("positivo");
+            }
+            if(numero < 0)
+            {
+                Console.WriteLine("negativo");
+            }
+            else if (numero == 0)
+            {
+                Console.WriteLine("zero");
+            }
+
         }
     }
 }
